a0233b39b425caeeace2c768b91bf23083c7b89d
Masoud Mehrabi <mmehrabigargari@b2soft.it>
2025-11-01 12:51:23 +0100

    - The Spring Data Rest dependency was used.

--- src/main/java/demo/user/dao/UserRepository.java
+++ src/main/java/demo/user/dao/UserRepository.java
@@ -1,8 +1,9 @@
 package demo.user.dao;
-
 import demo.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
